a58f269d5bce2a7d8c38a7ee4bd495b0e1733f82
Byron Botha <Byron.Botha@resbank.co.za>
2020-04-28 16:15:58 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spartadata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/spartadata/core_test.clj
@@ -0,0 +1,7 @@
+(ns spartadata.core-test
+  (:require [clojure.test :refer :all]
+            [spartadata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
