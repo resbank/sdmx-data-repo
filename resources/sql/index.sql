25bfdee338b14af3b9cc9e6556e7bbe5b5bc2aaf
Byron Botha <Byron.Botha@resbank.co.za>
2020-05-12 21:45:39 +0200

    Added index on obs_attr FK to speed up rollback

--- resources/sql/index.sql
+++ resources/sql/index.sql
@@ -1,3 +1,15 @@
+-- :name create-array-idx
+-- :command :execute
+-- :result :raw
+-- :doc Create a multi-column index on the :dimension_ids array.
+CREATE INDEX IF NOT EXISTS array_idx ON series (dimension_ids, dataset_id);
+
+-- :name drop-array-idx
+-- :command :execute
+-- :result :raw
+-- :doc Drop array_idx.
+DROP INDEX IF EXISTS array_idx;
+
 -- :name create-obs-idx
 -- :command :execute
 -- :result :raw
@@ -10,14 +22,16 @@ CREATE INDEX IF NOT EXISTS obs_idx ON observation (created, series_id);
 -- :doc Drop obs_idx.
 DROP INDEX IF EXISTS obs_idx;
 
--- :name create-array-idx
+-- :name create-obs-attr-idx
 -- :command :execute
 -- :result :raw
--- :doc Create a multi-column index on the :dimension_ids array.
-CREATE INDEX IF NOT EXISTS array_idx ON series (dimension_ids, dataset_id);
+-- :doc Create an index on the observation attribute table.
+CREATE INDEX IF NOT EXISTS obs_attr_idx ON observation_attribute (observation_id);
 
--- :name drop-array-idx
+-- :name drop-obs-attr-idx
 -- :command :execute
 -- :result :raw
--- :doc Drop array_idx.
-DROP INDEX IF EXISTS array_idx;
+-- :doc Drop obs_idx.
+DROP INDEX IF EXISTS obs_attr_idx;
+
+
